327a9a0ab0390e690f25339f7684396b3dd0ea54
Jim Lindblom <jim@sparkfun.com>
2015-06-22 12:04:54 -0600

    Initial commit of library. (v0.0.1).

---
+++ firmware/SparkFunMicroOLED.h
@@ -0,0 +1,198 @@
+/*
+    MicroOLED Arduino Library
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef SPARKFUN_MICRO_OLED_H
+#define SPARKFUN_MICRO_OLED_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include "application.h"
+
+#define swap(a, b) { uint8_t t = a; a = b; b = t; }
+#define _BV(x)	(1 << x)
+#define pgm_read_byte(x) (*(x))
+#define pgm_read_word(x) (*(x))
+#define pgm_read_float(x) (*(x))
+
+#define DC_DEFAULT		8
+#define RST_DEFAULT		7
+#define CS_DEFAULT		10
+#define MODE_DEFAULT	MODE_SPI
+
+#define I2C_ADDRESS_SA0_0 0b0111100
+#define I2C_ADDRESS_SA0_1 0b0111101
+#define I2C_COMMAND 0x00
+#define I2C_DATA 0x40
+
+#define BLACK 0
+#define WHITE 1
+
+#define LCDWIDTH			64
+#define LCDHEIGHT			48
+#define FONTHEADERSIZE		6
+
+#define NORM				0
+#define XOR					1
+
+#define PAGE				0
+#define ALL					1
+
+#define WIDGETSTYLE0			0
+#define WIDGETSTYLE1			1
+#define WIDGETSTYLE2			2
+
+#define SETCONTRAST 		0x81
+#define DISPLAYALLONRESUME 	0xA4
+#define DISPLAYALLON 		0xA5
+#define NORMALDISPLAY 		0xA6
+#define INVERTDISPLAY 		0xA7
+#define DISPLAYOFF 			0xAE
+#define DISPLAYON 			0xAF
+#define SETDISPLAYOFFSET 	0xD3
+#define SETCOMPINS 			0xDA
+#define SETVCOMDESELECT		0xDB
+#define SETDISPLAYCLOCKDIV 	0xD5
+#define SETPRECHARGE 		0xD9
+#define SETMULTIPLEX 		0xA8
+#define SETLOWCOLUMN 		0x00
+#define SETHIGHCOLUMN 		0x10
+#define SETSTARTLINE 		0x40
+#define MEMORYMODE 			0x20
+#define COMSCANINC 			0xC0
+#define COMSCANDEC 			0xC8
+#define SEGREMAP 			0xA0
+#define CHARGEPUMP 			0x8D
+#define EXTERNALVCC 		0x01
+#define SWITCHCAPVCC 		0x02
+
+// Scroll
+#define ACTIVATESCROLL 					0x2F
+#define DEACTIVATESCROLL 				0x2E
+#define SETVERTICALSCROLLAREA 			0xA3
+#define RIGHTHORIZONTALSCROLL 			0x26
+#define LEFT_HORIZONTALSCROLL 			0x27
+#define VERTICALRIGHTHORIZONTALSCROLL	0x29
+#define VERTICALLEFTHORIZONTALSCROLL	0x2A
+
+typedef enum CMD {
+	CMD_CLEAR,			//0
+	CMD_INVERT,			//1
+	CMD_CONTRAST,		//2
+	CMD_DISPLAY,		//3
+	CMD_SETCURSOR,		//4
+	CMD_PIXEL,			//5
+	CMD_LINE,			//6
+	CMD_LINEH,			//7
+	CMD_LINEV,			//8
+	CMD_RECT,			//9
+	CMD_RECTFILL,		//10
+	CMD_CIRCLE,			//11
+	CMD_CIRCLEFILL,		//12
+	CMD_DRAWCHAR,		//13
+	CMD_DRAWBITMAP,		//14
+	CMD_GETLCDWIDTH,	//15
+	CMD_GETLCDHEIGHT,	//16
+	CMD_SETCOLOR,		//17
+	CMD_SETDRAWMODE		//18
+} commCommand_t;
+
+typedef enum COMM_MODE{
+	MODE_SPI,
+	MODE_I2C
+} micro_oled_mode;
+
+class MicroOLED : public Print
+{
+public:
+	MicroOLED(micro_oled_mode mode = MODE_DEFAULT, uint8_t rst = RST_DEFAULT, uint8_t dc = DC_DEFAULT, uint8_t cs = CS_DEFAULT);
+
+	void begin(void);
+	virtual size_t write(uint8_t);
+
+	// RAW LCD functions
+	void command(uint8_t c);
+	void data(uint8_t c);
+	void setColumnAddress(uint8_t add);
+	void setPageAddress(uint8_t add);
+
+	// LCD Draw functions
+	void clear(uint8_t mode);
+	void clear(uint8_t mode, uint8_t c);
+	void invert(bool inv);
+	void contrast(uint8_t contrast);
+	void display(void);
+	void setCursor(uint8_t x, uint8_t y);
+	void pixel(uint8_t x, uint8_t y);
+	void pixel(uint8_t x, uint8_t y, uint8_t color, uint8_t mode);
+	void line(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1);
+	void line(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1, uint8_t color, uint8_t mode);
+	void lineH(uint8_t x, uint8_t y, uint8_t width);
+	void lineH(uint8_t x, uint8_t y, uint8_t width, uint8_t color, uint8_t mode);
+	void lineV(uint8_t x, uint8_t y, uint8_t height);
+	void lineV(uint8_t x, uint8_t y, uint8_t height, uint8_t color, uint8_t mode);
+	void rect(uint8_t x, uint8_t y, uint8_t width, uint8_t height);
+	void rect(uint8_t x, uint8_t y, uint8_t width, uint8_t height, uint8_t color , uint8_t mode);
+	void rectFill(uint8_t x, uint8_t y, uint8_t width, uint8_t height);
+	void rectFill(uint8_t x, uint8_t y, uint8_t width, uint8_t height, uint8_t color , uint8_t mode);
+	void circle(uint8_t x, uint8_t y, uint8_t radius);
+	void circle(uint8_t x, uint8_t y, uint8_t radius, uint8_t color, uint8_t mode);
+	void circleFill(uint8_t x0, uint8_t y0, uint8_t radius);
+	void circleFill(uint8_t x0, uint8_t y0, uint8_t radius, uint8_t color, uint8_t mode);
+	void drawChar(uint8_t x, uint8_t y, uint8_t c);
+	void drawChar(uint8_t x, uint8_t y, uint8_t c, uint8_t color, uint8_t mode);
+	void drawBitmap(void);
+	uint8_t getLCDWidth(void);
+	uint8_t getLCDHeight(void);
+	void setColor(uint8_t color);
+	void setDrawMode(uint8_t mode);
+
+	// Font functions
+	uint8_t getFontWidth(void);
+	uint8_t getFontHeight(void);
+	uint8_t getTotalFonts(void);
+	uint8_t getFontType(void);
+	uint8_t setFontType(uint8_t type);
+	uint8_t getFontStartChar(void);
+	uint8_t getFontTotalChar(void);
+
+	// LCD Rotate Scroll functions
+	void scrollRight(uint8_t start, uint8_t stop);
+	void scrollLeft(uint8_t start, uint8_t stop);
+	void scrollVertRight(uint8_t start, uint8_t stop);
+	void scrollVertLeft(uint8_t start, uint8_t stop);
+	void scrollStop(void);
+	void flipVertical(bool flip);
+	void flipHorizontal(bool flip);
+
+private:
+	uint8_t csPin, dcPin, rstPin;
+	uint8_t wrPin, rdPin, dPins[8];
+	volatile uint8_t *wrport, *wrreg, *rdport, *rdreg;
+	uint8_t wrpinmask, rdpinmask;
+	micro_oled_mode interface;
+	uint8_t foreColor,drawMode,fontWidth, fontHeight, fontType, fontStartChar, fontTotalChar, cursorX, cursorY;
+	uint16_t fontMapWidth;
+	static const unsigned char *fontsPointer[];
+
+	void setup(micro_oled_mode mode, uint8_t rst, uint8_t dc, uint8_t cs);
+
+	// Communication
+	void spiTransfer(uint8_t data);
+	void spiSetup();
+	void i2cSetup();
+	void i2cWrite(uint8_t address, uint8_t control, uint8_t data);
+};
+#endif
---
+++ firmware/examples/OLED-Cube.cpp
@@ -0,0 +1,143 @@
+/******************************************************************************
+  MicroOLED_Hello.ino
+  SFE_MicroOLED Hello World Demo
+  Jim Lindblom @ SparkFun Electronics
+  Original Creation Date: June 22, 2015
+
+  This sketch draws a 3-D cube on the SparkFun Photon Micro OLED shield. 
+  It'll rotate as fast as it can.
+
+  Hardware Connections:
+    We'll be using the SPI interface on the MicroOLED, though it
+    also supports I2C (and a really messy parallel). If you want
+    to swap in I2C, read through the comments to find out how.
+	
+	(The Shield already does this for you, but a SparkFun Micro OLED will
+	also work with this library.)
+
+    MicroOLED ------------- Photon
+      GND ------------------- GND
+      VDD ------------------- 3.3V (VCC)
+    D1/MOSI ----------------- A5 (don't change)
+    D0/SCK ------------------ A3 (don't change)
+      D2
+      D/C ------------------- D4 (can be any digital pin)
+      RST ------------------- D7 (can be any digital pin)
+      CS  ------------------- A2 (can be any digital pin)
+
+  Development environment specifics:
+  	IDE: Particle Build
+  	Hardware Platform: Particle Photon
+                       SparkFun Photon Micro OLED Shield
+
+  This code is beerware; if you see me (or any other SparkFun
+  employee) at the local, and you've found our code helpful,
+  please buy us a round!
+
+  Distributed as-is; no warranty is given.
+*******************************************************************************/
+#include "SparkFunMicroOLED.h"  // Include the SFE_MicroOLED library
+#include "math.h"
+
+//////////////////////////
+// MicroOLED Definition //
+//////////////////////////
+#define PIN_RESET D7  // Connect RST to pin 7 (req. for SPI and I2C)
+#define PIN_DC    D3  // Connect DC to pin 3 (required for SPI)
+#define PIN_CS    A2 // Connect CS to pin A2 (required for SPI)
+
+//////////////////////////////////
+// MicroOLED Object Declaration //
+//////////////////////////////////
+// Declare a MicroOLED object. The parameters include:
+// 1 - Mode: Should be either MODE_SPI or MODE_I2C
+// 2 - Reset pin: Any digital pin
+// 3 - D/C pin: Any digital pin (SPI mode only)
+// 4 - CS pin: Any digital pin (SPI mode only, 10 recommended)
+MicroOLED oled(MODE_SPI, PIN_RESET, PIN_DC, PIN_CS);
+//MicroOLED uOLED(MODE_I2C, PIN_RESET); // Example I2C declaration
+
+// I2C is great, but will result in a much slower update rate. The
+// slower framerate may be a worthwhile tradeoff, if you need more
+// pins, though.
+
+////////////////////
+// Cube Variables //
+////////////////////
+#define SHAPE_SIZE 600
+#define ROTATION_SPEED 0 // ms delay between cube draws
+
+int SCREEN_WIDTH = oled.getLCDWidth();
+int SCREEN_HEIGHT = oled.getLCDHeight();
+
+float d = 3;
+float px[] = { -d,  d,  d, -d, -d,  d,  d, -d };
+float py[] = { -d, -d,  d,  d, -d, -d,  d,  d };
+float pz[] = { -d, -d, -d, -d,  d,  d,  d,  d };
+
+float p2x[] = {0,0,0,0,0,0,0,0};
+float p2y[] = {0,0,0,0,0,0,0,0};
+
+float r[] = {0,0,0};
+
+
+void setup()
+{
+  // These three lines of code are all you need to initialize the
+  // OLED and print the splash screen.
+
+  // Before you can start using the OLED, call begin() to init
+  // all of the pins and configure the OLED.
+  oled.begin();
+  // clear(ALL) will clear out the OLED's graphic memory.
+  // clear(PAGE) will clear the Arduino's display buffer.
+  oled.clear(ALL);  // Clear the display's memory (gets rid of artifacts)
+  // To actually draw anything on the display, you must call the
+  // display() function.
+}
+
+void loop()
+{
+	drawCube();
+	delay(ROTATION_SPEED);
+}
+
+void drawCube()
+{
+  r[0]=r[0]+M_PI/180.0; // Add a degree
+	r[1]=r[1]+M_PI/180.0; // Add a degree
+	r[2]=r[2]+M_PI/180.0; // Add a degree
+	if (r[0] >= 360.0*M_PI/180.0) r[0] = 0;
+	if (r[1] >= 360.0*M_PI/180.0) r[1] = 0;
+	if (r[2] >= 360.0*M_PI/180.0) r[2] = 0;
+
+	for (int i=0;i<8;i++)
+	{
+		float px2 = px[i];
+		float py2 = cos(r[0])*py[i] - sin(r[0])*pz[i];
+		float pz2 = sin(r[0])*py[i] + cos(r[0])*pz[i];
+
+		float px3 = cos(r[1])*px2 + sin(r[1])*pz2;
+		float py3 = py2;
+		float pz3 = -sin(r[1])*px2 + cos(r[1])*pz2;
+
+		float ax = cos(r[2])*px3 - sin(r[2])*py3;
+		float ay = sin(r[2])*px3 + cos(r[2])*py3;
+		float az = pz3-150;
+
+		p2x[i] = SCREEN_WIDTH/2+ax*SHAPE_SIZE/az;
+		p2y[i] = SCREEN_HEIGHT/2+ay*SHAPE_SIZE/az;
+	}
+
+	oled.clear(PAGE);
+	for (int i=0;i<3;i++)
+	{
+		oled.line(p2x[i],p2y[i],p2x[i+1],p2y[i+1]);
+		oled.line(p2x[i+4],p2y[i+4],p2x[i+5],p2y[i+5]);
+		oled.line(p2x[i],p2y[i],p2x[i+4],p2y[i+4]);
+	}
+	oled.line(p2x[3],p2y[3],p2x[0],p2y[0]);
+	oled.line(p2x[7],p2y[7],p2x[4],p2y[4]);
+	oled.line(p2x[3],p2y[3],p2x[7],p2y[7]);
+	oled.display();
+}
